feb072a3a6ea235b35076a198c1ffc46aa83d029
Ahmed Tariq <ahmed.tm097@gmail.com>
2021-11-28 14:02:38 +0500

    Algo

---
+++ MultiLinearRegression.py
@@ -0,0 +1,64 @@
+# Multiple Linear Regression without any elimination
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import statsmodels.api as sm
+
+# Importing the dataset
+dataset = pd.read_csv('Real estate.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+print(X)
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+print(X_train)
+
+# Training the Multiple Linear Regression model on the Training set
+from sklearn.linear_model import LinearRegression
+
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = regressor.predict(X_test)
+
+##Apply Backward elimination to achieve optimal model
+import statsmodels.formula.api as sm
+
+# add a feature to the matrix X that carries value 1
+# this 1 is actually the value of x_0 variable for b_0 the y intercept.
+# X = np.append(arr = X, values = np.ones((50,1)).astype(int), axis = 1)
+# This will add the 1's column to the end of the matrix, but we want it added as the first column
+# so we swap the existing matrix with 1's column in the append fucntion
+print(X)
+X = np.append(arr=np.ones((50, 1)).astype(int), values=X, axis=1)
+print(X)
+
+## For backward elimination we need to create another optimal matrix and initiate with matrix X
+X_opt = X[:, [0, 1, 2, 3, 4, 5]]
+print(X_opt)
+print(y)
+
+# The regressor that supports backword elimination comes through the statsmodel api
+import statsmodels.api as sm
+
+regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()
+regressor_OLS.summary()
+
+X_opt = X[:, [0, 2, 3, 4, 5]]
+import statsmodels.api as sm
+
+regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()
+regressor_OLS.summary()
+
+X_opt = X[:, [0, 2, 4, 5]]
+import statsmodels.api as sm
+
+regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()
+regressor_OLS.summary()
+X_opt
---
+++ Preprocessing.py
@@ -0,0 +1,93 @@
+# Get/Download Dataset
+# Import Libraries
+# Import Data Set into memory objects
+# Fill/Handle Missing Data (if there is missing data)
+# Make the data Categorical --> converting text values to numeric -- categorical 1,2,3 -OR- one hot encoding
+# Split data into Training and Test sets --> X_train, X_test, y_train, y_test
+# Feature Scaling --> bringing all features values in comparable ranges say (0,1)
+# Data Processing --> Good to go for data processing
+
+import numpy as np
+import pandas as pd
+
+dataset = pd.read_csv('Data/heart.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 13].values
+
+print('Displaying Data for X \n', X)
+print('\n Displaying Data for y \n', y)
+
+# dealing with the missing values
+# Imputer class has been deprecated in newer versions so better use SimpleImputer
+from sklearn.impute import SimpleImputer
+
+imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+# axis 0 means mean of column, axi =1 means mean of row
+# strategy mean, median, most_frequent
+imputer = imputer.fit(X[:, 1:13])
+X[:, 1:13] = imputer.transform(X[:, 1:13])
+print('Filling the value using Mean method: \n')
+print('Displaying Data for X \n', X)
+print('\n Displaying Data for y \n', y)
+
+imputer = SimpleImputer(missing_values=np.nan, strategy='median')
+imputer = imputer.fit(X[:, 1:13])
+print('\n Splitting the Values In X \n')
+print('Displaying Data for X \n', X)
+X[:, 1:13] = imputer.transform(X[:, 1:13])
+print('Filling the value using Median method: \n')
+print('Displaying Data for X \n', X)
+print('\n Displaying Data for y \n', y)
+
+imputer = SimpleImputer(missing_values=np.nan, strategy='most_frequent')
+imputer = imputer.fit(X[:, 1:13])
+print(' Splitting the Values In X \n')
+print(' Displaying Data for X \n', X)
+X[:, 1:13] = imputer.transform(X[:, 1:13])
+print('Filling the value using Most Frequent method: \n')
+print(' Displaying Data for X \n', X)
+print('\n Displaying Data for y \n', y)
+
+# Encoding categorical data
+
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+
+labelEncoder_X = LabelEncoder()
+X[:, 0] = labelEncoder_X.fit_transform(X[:, 0])
+print('\n Displaying Data in X after Encoding the first column \n', X)
+
+# Thus, we should also use OneHotEncoding by adding dummy columns as per number of distinct values in column country
+onehotencoder = OneHotEncoder()
+X = onehotencoder.fit_transform(X).toarray()
+labelEncoder_y = LabelEncoder()
+y = labelEncoder_y.fit_transform(y)
+print('\n Data After Encoding and Sorting the column \n')
+print('Displaying Data for X \n', X)
+print('\n Displaying Data for y \n', y)
+
+
+# Splitting data set into training and test sets
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+# X for independent variables and y for dependent variables
+# random_state for random sampling
+# we can also use stratified sampling (almost equal number of representation for each class)
+
+
+# Feature Scaling What is feature scaling Why is it important How is it done theoretically how can we implement it in
+# Python Age is between 20s and 50s, whereas salary is between 40K - 80K In this case salary becomes overwhelming
+# attribute, Euclidean distance will be computed by salary with a little effect of age x_stand= x - mean(
+# x)/standard_deviation(x) 44 - 37.6/4.1 OR x_norm  = x - min(x)/max(x)- min(x) max(x) --> 1 min(x) --> 0
+
+from sklearn.preprocessing import StandardScaler
+
+sc_X = StandardScaler()
+print('\n Training data in X  \n ', X_train)
+X_train = sc_X.fit_transform(X_train)
+print('\n Training data in X  \n ', X_train)
+print('\n Testing data in X  \n ', X_test)
+X_test = sc_X.transform(X_test)
+print('\n Testing data in X  \n ', X_test)
+# for classification problems we do not need to apply feature scaling on test variable, however for regression we
+# should apply it
